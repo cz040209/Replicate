6b4d21688de665399355688b224c0a996f18b105
cz040209 <leecz-wm23@student.tarc.edu.my>
2024-12-18 21:41:10 +0800

    Update replicate.py

--- replicate.py
+++ replicate.py
@@ -1,10 +1,3 @@
-import sys
-import streamlit as st
-
-st.write(f"Python version used by Streamlit: {sys.version}")
-st.write(f"Python executable used by Streamlit: {sys.executable}")
-
-
 import os
 import streamlit as st
 import pdfplumber
