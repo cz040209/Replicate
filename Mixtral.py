838b342b1062a5e6d7961a086d584566504a3f26
cz040209 <leecz-wm23@student.tarc.edu.my>
2024-12-19 15:09:34 +0800

    Update Mixtral.py

--- Mixtral.py
+++ Mixtral.py
@@ -8,7 +8,7 @@ import pytesseract
 from PIL import Image
 
 # Set the path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Custom CSS for a more premium look
 st.markdown("""
