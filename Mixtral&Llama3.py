2f14aa4a3a3abfc25459cad055937c0258d80d93
cz040209 <leecz-wm23@student.tarc.edu.my>
2024-12-21 18:07:41 +0800

    Update Mixtral&Llama3.py

--- Mixtral&Llama3.py
+++ Mixtral&Llama3.py
@@ -112,7 +112,7 @@ headers = {
 # Available models, including the two new Sambanova models
 available_models = {
     "Mixtral 8x7b": "mixtral-8x7b-32768",
-    "Llama 3.3 70b Versatile": "llama-3.3-70b-versatile",
+    "Llama 3.1 70b Versatile": "llama-3.1-70b-versatile",
     "Qwen 2.5-72B-Instruct": "sambanova-qwen-2.5-72b-instruct",
     "Meta-Llama 3.2-1B-Instruct": "sambanova-meta-llama-3.2-1b-instruct"
 }
